7aa28ffd24c07880f04ce82d18fd203c581aa144
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-06-23 12:12:55 -0400

    FIrst commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import bday from "./bday.png";
+
+function Header(props) {
+  return (
+    <header>
+      <h2>Happy Birthday {props.firstName}!</h2>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section className="main">
+      <img
+        src={bday}
+        alt="bubbly letters that say Happy Birthday"
+        height={400}
+      />
+      <p>Here's what we got you buddy:</p>
+      <ul id="menu">
+        {" "}
+        {props.gifts.map((gift) => (
+          <li key={gift.id}>{gift.title}</li>
+        ))}{" "}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
+
+const gifts = [
+  "Motorcycle!",
+  "Esso Gift Card",
+  "Wu Tang T Shirt",
+  "Comfy Socks",
+];
+
+const giftObjects = gifts.map((gift, i) => ({ id: i, title: gift }));
+console.log(giftObjects);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header firstName="Chris" lastName="Simpson" age="42" />
+      <Main adjective="freaking" gifts={giftObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
